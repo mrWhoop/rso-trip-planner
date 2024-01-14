efd5ec2214f385c7979d235c5002086a42daaacf
Robi <modic.robert@gmail.com>
2024-01-14 19:35:00 +0100

    openAPI added servers

--- api/src/main/java/si/fri/rso/samples/locations/api/v1/ImageMetadataApplication.java
+++ api/src/main/java/si/fri/rso/samples/locations/api/v1/ImageMetadataApplication.java
@@ -13,7 +13,7 @@ import org.eclipse.microprofile.openapi.annotations.servers.Server;
 @OpenAPIDefinition(info = @Info(title = "Trip planner location API", version = "v1",
         contact = @Contact(email = "rm1052@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing location data."),
-        servers = @Server(url = "http://localhost:8080/"))
+        servers = {@Server(url = "http://localhost:8080/"), @Server(url = "http://20.75.151.253:8080/")})
 @ApplicationPath("/v1")
 public class ImageMetadataApplication extends Application {
 
